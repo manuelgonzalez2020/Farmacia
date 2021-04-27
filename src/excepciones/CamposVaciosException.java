241c75a496fe71d89d56f50f1f7d6bee078ed2a2
Manuel González <manuelgonzalezvela@cdpjosecabrera.es>
2021-04-27 16:51:13 +0200

    .

--- src/excepciones/CamposVaciosException.java
+++ src/excepciones/CamposVaciosException.java
@@ -3,6 +3,6 @@ public class CamposVaciosException extends Exception{
 	private static final long serialVersionUID = 1L;
 	public CamposVaciosException(String campo) {
 		// TODO Auto-generated constructor stub
-		super("El campo "+campo+" está vacío hijo de las 7 mil putas");
+		super("El campo "+campo+" está vacío");
 	}
 }
